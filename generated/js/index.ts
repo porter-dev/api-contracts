7dc160e4a854607b74db35bf75254ac762643492
Stefan McShane <stefanmcshane@users.noreply.github.com>
2023-03-09 20:14:46 -0500

    Releasing v0.0.23

--- generated/js/index.ts
+++ generated/js/index.ts
@@ -3,6 +3,6 @@ export * from "./src/porter/v1/cluster_control_plane_pb";
 export * from "./src/porter/v1/cluster_pb";
 export * from "./src/porter/v1/eks_pb";
 export * from "./src/porter/v1/gke_pb";
-export * from "./src/porter/v1/porter_pb";
+export * from "./src/porter/v1/contract_pb";
 export * from "./src/porter/v1/status_messages_pb";
 export * from "./src/porter/v1/system_applications_pb";
