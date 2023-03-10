93d9dfd7f9a2f5e00a9a102eb48ffd60bbed5534
Stefan McShane <stefanmcshane@users.noreply.github.com>
2023-03-10 11:46:26 -0500

    Releasing v0.0.39

--- generated/go/helpers/helpers.go
+++ generated/go/helpers/helpers.go
@@ -10,17 +10,23 @@ import (
 	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
-// UnmarshalContractObject is a convenience method for returning a protobuf contract object from any
+// UnmarshalContractObjectFromReader is a convenience method for returning a protobuf contract object from any
 // io.Reader, such as a request body, and will work for any part of a contract, or contract revision
-func UnmarshalContractObject(r io.Reader, contract protoreflect.ProtoMessage) error {
+func UnmarshalContractObjectFromReader(r io.Reader, contract protoreflect.ProtoMessage) error {
 	by, err := io.ReadAll(r)
 	if err != nil {
 		return fmt.Errorf("unable to read body for contract revision")
 	}
 
-	err = protojson.Unmarshal(by, contract)
+	return UnmarshalContractObject(by, contract)
+}
+
+// UnmarshalContractObject is a convenience method for returning a protobuf contract object from bytes
+// and will work for any part of a contract, or contract revision
+func UnmarshalContractObject(by []byte, contract protoreflect.ProtoMessage) error {
+	err := protojson.Unmarshal(by, contract)
 	if err != nil {
-		return fmt.Errorf("unable to convert reader into contract")
+		return fmt.Errorf("unable to convert bytes into contract")
 	}
 
 	if contract == nil {
