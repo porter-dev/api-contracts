8c9b3e3575796936b60fbfd950b85a6eb0708497
jose-fully-ported <141160579+jose-fully-ported@users.noreply.github.com>
2023-11-14 17:03:45 -0500

    feat: add proto for soc2 settings (#111)
    - Soc2 can be enabled at a cluster level, which overrides any falsy values on the contract
    - EKS can have all of it's cloudwatch logs shipped to a cluster-specific s3 bucket
    - ECR registries associated with an EKS cluster can have their registries made SOC2 compliant

--- generated/js/src/porter/v1/cluster_pb.d.ts
+++ generated/js/src/porter/v1/cluster_pb.d.ts
@@ -124,6 +124,13 @@ export declare class Cluster extends Message<Cluster> {
     case: "aksKind";
   } | { case: undefined; value?: undefined };
 
+  /**
+   * soc2_compliant force enables all the various soc2-related fields on a cluster
+   *
+   * @generated from field: bool soc2_compliant = 9;
+   */
+  soc2Compliant: boolean;
+
   constructor(data?: PartialMessage<Cluster>);
 
   static readonly runtime: typeof proto3;
